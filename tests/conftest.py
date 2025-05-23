581f72065187b8ff35811ae58cb31122f6e6ee33
renan-asantos <renan_assis97@hotmail.com>
2025-05-22 22:44:51 -0300

    Exercicio aula 04

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,18 +25,21 @@ def session():
 
     table_registry.metadata.drop_all(engine)
 
+
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
-
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
 
     event.listen(model, 'before_insert', fake_time_hook)
 
     yield time
 
     event.remove(model, 'before_insert', fake_time_hook)
+
 
 @pytest.fixture
 def mock_db_time():
--- tests/test_db.py
+++ tests/test_db.py
@@ -7,7 +7,9 @@ from fast_zero.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(username='alice', password='secret', email='teste@test')
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
         session.add(new_user)
         session.commit()
 
@@ -19,4 +21,5 @@ def test_create_user(session, mock_db_time):
         'password': 'secret',
         'email': 'teste@test',
         'created_at': time,
+        'updated_at': time,
     }
